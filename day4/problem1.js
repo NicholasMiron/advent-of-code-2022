5be2deb5ebd5e04b8a13ee265b0a842ad72f521c
Nicholas Miron <nicholasmiron@gmail.com>
2022-12-05 23:51:46 -0600

    day4

---
+++ day4/problem1.js
@@ -0,0 +1,17 @@
+const sample1 = require('fs').readFileSync(require('path').resolve(__dirname, './inputs/sample1.txt'), 'utf8');
+const input1 = require('fs').readFileSync(require('path').resolve(__dirname, './inputs/input1.txt'), 'utf8');
+
+const solution = (input) => {
+  return input
+    .split('\n')
+    .map((pair) => pair.split(','))
+    .map((ranges) => ranges.map((range) => range.split('-').map(Number)))
+    .filter(([one, two]) => {
+      const [start1, end1] = one;
+      const [start2, end2] = two;
+      return (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)
+    }).length;
+};
+
+console.log('sample:', solution(sample1));
+console.log('solution:', solution(input1));
