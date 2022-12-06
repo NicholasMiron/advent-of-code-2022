60802196e14181b41219269395e53ef2edb06beb
Nicholas Miron <nicholasmiron@gmail.com>
2022-12-05 23:52:03 -0600

    day6

---
+++ day6/problem2.js
@@ -0,0 +1,15 @@
+const sample1 = require('fs').readFileSync(require('path').resolve(__dirname, './inputs/sample1.txt'), 'utf8');
+const input1 = require('fs').readFileSync(require('path').resolve(__dirname, './inputs/input1.txt'), 'utf8');
+
+const solution = (input) => {
+  const distinctSize = 14;
+  for (let i = 0; i <= input.length - distinctSize; i++) {
+    const set = new Set(input.slice(i, i + distinctSize));
+    if (set.size === distinctSize) {
+      return i + distinctSize;
+    }
+  }
+};
+
+console.log('sample:', solution(sample1));
+console.log('solution:', solution(input1));
